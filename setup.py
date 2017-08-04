cb5b0c0952f3313c7d8b0735ed6cadfb82da3f2c
Sydney Weidman <s.weidman@uwinnipeg.ca>
2017-08-04 08:13:37 -0500

    Resolved conflict in setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,7 @@ execfile('dice/version.py')
 
 setup(
     name="dice",
-<<<<<<< Updated upstream
     version="0.1.1",
     packages=["dice"],
     test_suite='dice.tests',
-    entry_points={
-        'console_scripts': [
-            'roll=dice.command:main'
-        ]
-    }
-)
-=======
-    version=__version__,
-    packages=["dice"],
-    data_files=[('appconfig', ['resources/reader.ini']),
-                ('logconfig', ['resources/log.ini'])],
-    test_suite='dice.tests', )
->>>>>>> Stashed changes
+    entry_points={'console_scripts': ['roll=dice.command:main']})
